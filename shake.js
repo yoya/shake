faad2b802e0c766606ab71b4693aa29b1df1421a
Yoshihiro Yamazaki <yoya@awm.jp>
2016-06-05 22:12:50 +0900

    - ベルの音を追加 - 音量や音程の揺れを作った (同じ音が鳴り続けるのが味気なかったので)

--- shake.js
+++ shake.js
@@ -27,38 +27,54 @@ var getAudioBuffer = function(url, fn) {
 };
 
 // サウンドを再生
-var playSound = function() {
+var soundmax = document.getElementById("soundmax");
+var soundmin = document.getElementById("soundmin");
+var playSound = function(volume) {
   // source を作成
   var source = context.createBufferSource();
+  var gain = context.createGain();
   // buffer をセット
   source.buffer = buffer;
+  if (soundmax && soundmin) {
+      max = parseFloat(soundmax.value);
+      min = parseFloat(soundmin.value);
+      source.playbackRate.value = min + Math.random() * (max - min);
+  } else {
+      source.playbackRate.value = 1.0
+  }
+  gain.gain.value = volume;
   // context に connect
-  source.connect(context.destination);
+  source.connect(gain);
+  gain.connect(context.destination);
   // 再生
   source.start(0);
 };
 
 // main
+var soundfile = document.getElementById("soundfile");
 window.onload = function() {
   // サウンドを読み込む
-  var soundfile = document.getElementById("soundfile");
   getAudioBuffer(soundfile.value, function(b) { buffer = b; });
 };
 
 document.addEventListener("touchstart", touch, false);
 
 function touch() {
-    console.log("touch");
-    playSound();
+    // console.debug("touch");
+    playSound(1);
 }
 
 window.addEventListener("devicemotion", motion);
 function motion(ev) {
     var ax = ev.accelerationIncludingGravity.x;
     var ay = ev.accelerationIncludingGravity.y;
-    if (ax*ax+ay*ay > 1000) {
-	console.log("shake");
-	playSound();
+    var power = ax*ax+ay*ay;
+    if (power  > 1000) {
+	console.debug("shake:" + power);
+	if (power > 4000) {
+	    power = 4000;
+	}
+	playSound((power - 1000) / 3000);
     }
 }
 
